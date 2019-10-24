bb59b8e05a1b5847d7affce9ff5ce55062af8c0e
SimlaS <slawo4her@tlen.pl>
2019-10-24 15:49:01 +0200

    6 files modified, 3 added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -62,4 +62,25 @@ export class HeroService {
     );
   }
 
+  deleteHero (hero: Hero): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`usunięto hero id=${id}`)),
+      catchError(this.handleError<Hero>('usunięto Hero'))
+    );
+  }
+
+    /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+      if (!term.trim()) {
+        // if not search term, return empty hero array.
+        return of([]);
+      }
+      return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+        tap(_ => this.log(`found heroes matching "${term}"`)),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+      );
+  }
+
 }
